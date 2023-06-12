4fb5cc3c69a8a5cfef5767c36fc72eed4ec1f93c
Lucas Doell <lmd248@cornell.edu>
2023-06-11 20:39:19 -0400

    add parsing to repl

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 // PROMPT is the prompt for the REPL
@@ -24,10 +24,24 @@ func Start(in io.Reader, out io.Writer) {
 		// line is the input from the user
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		// tok is the token from the lexer
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) > 0 {
+			// print parser errors
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
